92d9647a5ef1ce130266d13293df654548de13a7
Gosha Semenyak <gsemenyak@ispras.ru>
2023-02-21 17:55:19 +0300

    Add database creation

--- src/main/resources/sql/create_database.sql
+++ src/main/resources/sql/create_database.sql
@@ -1,67 +1,35 @@
-DROP SCHEMA IF EXISTS schedule CASCADE;
-CREATE SCHEMA schedule;
+DROP SCHEMA IF EXISTS relationship CASCADE;
+CREATE SCHEMA relationship;
 
-CREATE TABLE schedule.groups (
-    group_id SERIAL PRIMARY KEY,
-    faculty_name text NOT NULL,
-    course integer CHECK(course >= 1 AND course <= 6),
-    group_number text NOT NULL
+CREATE TABLE relationship.residence (
+    residence_id SERIAL PRIMARY KEY,
+    country text NOT NULL,
+	town text NOT NULL,
+	address text NOT NULL,
+	description text
 );
 
-CREATE TABLE schedule.students (
-    student_id SERIAL PRIMARY KEY,
+CREATE TABLE relationship.person (
+    person_id SERIAL PRIMARY KEY,
     surname text NOT NULL,
     name text NOT NULL,
     patronymic text,
-    group_id integer CONSTRAINT student_group REFERENCES schedule.groups ON DELETE CASCADE
+	gender text NOT NULL,
+	birth_date timestamp NOT NULL,
+	death_date timestamp,
+	characteristics text
 );
 
-CREATE TABLE schedule.subjects (
-    subject_id SERIAL PRIMARY KEY,
-    subject_name text NOT NULL,
-    faculty_name text
+CREATE TABLE relationship.person_residence (
+	person_id integer REFERENCES relationship.person ON DELETE CASCADE,
+	residence_id integer REFERENCES relationship.residence ON DELETE CASCADE
 );
 
-CREATE TABLE schedule.examiners (
-    examiner_id SERIAL PRIMARY KEY,
-    surname text NOT NULL,
-    name text NOT NULL,
-    patronymic text
+CREATE TABLE relationship.relation (
+    relation_id SERIAL PRIMARY KEY,
+    first_person_id integer REFERENCES relationship.person ON DELETE CASCADE,
+	second_person_id integer REFERENCES relationship.person ON DELETE CASCADE,
+	relation_type text NOT NULL,
+	information text
 );
 
-CREATE TABLE schedule.timedate (
-    timedate_id SERIAL PRIMARY KEY,
-    building_name text NOT NULL,
-    audience_num text NOT NULL,
-    "date" timestamp NOT NULL
-);
-
-CREATE TABLE schedule.consultation (
-    consultation_id SERIAL PRIMARY KEY,
-    timedate_id integer CONSTRAINT cons_timedate REFERENCES schedule.timedate
-);
-
-CREATE TABLE schedule.exams (
-    exam_id SERIAL PRIMARY KEY,
-    subject_id integer CONSTRAINT exam_subject REFERENCES schedule.subjects ON DELETE CASCADE,
-    timedate_id integer CONSTRAINT exam_timedate REFERENCES schedule.timedate ON UPDATE CASCADE,
-    consultation_id integer DEFAULT NULL CONSTRAINT exam_consultation REFERENCES schedule.consultation ON DELETE SET NULL
-);
-
-CREATE TABLE schedule.groups_exams (
-    exam_id integer CONSTRAINT gre_exam REFERENCES schedule.exams ON DELETE CASCADE,
-    group_id integer CONSTRAINT gre_group REFERENCES schedule.groups ON DELETE CASCADE,
-    UNIQUE (exam_id, group_id)
-);
-
-CREATE TABLE schedule.exams_examiners (
-    exam_id integer CONSTRAINT ee_exam REFERENCES schedule.exams ON DELETE CASCADE,
-    examiner_id integer CONSTRAINT ee_examiner REFERENCES schedule.examiners ON DELETE CASCADE,
-    UNIQUE (exam_id, examiner_id)
-);
-
-CREATE TABLE schedule.examiners_subjects (
-    subject_id integer CONSTRAINT es_subject REFERENCES schedule.subjects ON DELETE CASCADE,
-    examiner_id integer CONSTRAINT es_examiner REFERENCES schedule.examiners ON DELETE CASCADE,
-    UNIQUE (subject_id, examiner_id)
-);
